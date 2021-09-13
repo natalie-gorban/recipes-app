0d52f148a8a17dbf87ec81b3ea72e29d2e04d82a
Natalie Gorban <natalie.v.gorban@gmail.com>
2021-09-13 16:01:31 +0300

    working add-recipe (front end)

--- client/src/Components/NewRecipe/NewRecipe.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import NewRecipe from './NewRecipe';
-
-test('renders NewRecipe', () => {
-  render(<NewRecipe />);
-  const linkElement = screen.getByText(/New Recipe/i);
-  expect(linkElement).toBeInTheDocument();
-});
