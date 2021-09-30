2f823dd724bb35ccabd3eea4ba94ba419e4858ad
Natalie Gorban <natalie.v.gorban@gmail.com>
2021-09-30 10:47:29 +0300

    replace image

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,3 @@
 export const CDN_URL = process.env.REACT_APP_CDN_URL;
 export const API_URL = `${process.env.API_URL || "http://localhost:5000/api/"}`;
-export const DEFAULT_IMAGE_NAME = "static/burger.jpg"
+export const DEFAULT_IMAGE_NAME = "static/no-image.jpg"
