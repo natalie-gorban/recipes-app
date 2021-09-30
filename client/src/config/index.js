aa3f79c5d60617d57d5e25c3e22524044f66b686
Natalie Gorban <natalie.v.gorban@gmail.com>
2021-09-30 11:59:28 +0300

    replace empty image name

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,3 @@
 export const CDN_URL = process.env.REACT_APP_CDN_URL;
 export const API_URL = `${process.env.API_URL || "http://localhost:5000/api/"}`;
-export const DEFAULT_IMAGE_NAME = "static/no-image.jpg"
+export const DEFAULT_IMAGE_NAME = "static/images/empty-image.jpg";
